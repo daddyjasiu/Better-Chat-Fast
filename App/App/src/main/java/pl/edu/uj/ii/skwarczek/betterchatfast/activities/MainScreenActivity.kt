50f79829d1775d39974e0aac09a39ae5d9388b80
Jan Skwarczek <jasiu.skwarczek@gmail.com>
2022-01-20 19:24:33 +0100

    authentication login works with google oauth2

---
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package pl.edu.uj.ii.skwarczek.betterchatfast.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.edu.uj.ii.skwarczek.betterchatfast.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
--- App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/SignInActivity.kt
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/SignInActivity.kt
@@ -1,13 +1,23 @@
 package pl.edu.uj.ii.skwarczek.betterchatfast.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import pl.edu.uj.ii.skwarczek.betterchatfast.R
 import pl.edu.uj.ii.skwarczek.betterchatfast.adapters.SignInAdapter
 
@@ -21,23 +31,88 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var signInPasswordField: EditText
     private lateinit var googleActionButton: FloatingActionButton
 
+    private lateinit var auth: FirebaseAuth
+    private companion object{
+        private  const val TAG = "SignInActivity"
+        private const val RC_GOOGLE_SIGN_IN = 2115
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
 
         initView()
 
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val client = GoogleSignIn.getClient(this, gso)
+
         googleActionButton.setOnClickListener{
-            googleButtonClicked()
+            val signInIntent = client.signInIntent
+            startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
         }
     }
 
-    private fun googleButtonClicked(){
-        println("SWAGGERS")
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser == null){
+            Log.w(TAG,"User is null, not going to navigate")
+        }
+        else{
+            startActivity(Intent(this, MainScreenActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed!", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
     }
 
     private fun initView() {
 
+        auth = Firebase.auth
         googleActionButton = findViewById(R.id.fab_google)
 
         tabLayout = findViewById(R.id.tab_layout)
