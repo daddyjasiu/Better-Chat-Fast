a0ad624c4c06a53af976c7811376b26323cda663
Jan Skwarczek <jasiu.skwarczek@gmail.com>
2022-01-20 16:39:05 +0100

    login screen created with sign in and sign up tabs

---
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/SignInActivity.kt
@@ -0,0 +1,51 @@
+package pl.edu.uj.ii.skwarczek.betterchatfast.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+import pl.edu.uj.ii.skwarczek.betterchatfast.R
+import pl.edu.uj.ii.skwarczek.betterchatfast.adapters.SignInAdapter
+import kotlin.random.Random
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+    private lateinit var signUpEmailField: EditText
+    private lateinit var signUpPasswordField: EditText
+    private lateinit var signInEmailField: EditText
+    private lateinit var signInPasswordField: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        initView()
+
+    }
+
+    private fun initView() {
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager = findViewById(R.id.view_pager)
+
+        val tabTitles = arrayOf("Sign in", "Sign up")
+
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.addTab(tabLayout.newTab())
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        viewPager.adapter =
+            SignInAdapter(supportFragmentManager, lifecycle, this, tabLayout.tabCount)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+
+}
