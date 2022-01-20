3d10dc2625000f8ea3355f3cee3409a1d0877b42
Jan Skwarczek <jasiu.skwarczek@gmail.com>
2022-01-20 19:45:26 +0100

    users can now logout using google signOut

--- App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/MainScreenActivity.kt
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/activities/MainScreenActivity.kt
@@ -1,12 +1,37 @@
 package pl.edu.uj.ii.skwarczek.betterchatfast.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import pl.edu.uj.ii.skwarczek.betterchatfast.R
+import kotlin.math.sign
 
 class MainScreenActivity : AppCompatActivity() {
+
+    private lateinit var signOutButton: Button
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+
+        initView()
+
+        signOutButton.setOnClickListener {
+            auth.signOut()
+            val signOutIntent = Intent(this, SignInActivity::class.java)
+            signOutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(signOutIntent)
+        }
+    }
+
+    private fun initView(){
+        auth = Firebase.auth
+        signOutButton = findViewById(R.id.signout_button)
     }
 }
