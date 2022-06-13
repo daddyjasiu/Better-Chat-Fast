d9334a2c7211217dedc5778eedf3c6d4c32d9739
Sou00 <igorlonak@gmail.com>
2022-06-13 18:20:36 +0200

    Uploading custom user avatars
    not working yet, have to rescale the images to 94x94

--- App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/onboarding/Onboarding3Fragment.kt
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/onboarding/Onboarding3Fragment.kt
@@ -1,19 +1,94 @@
 package pl.edu.uj.ii.skwarczek.betterchatfast.onboarding
 
+import android.app.Activity
+import android.content.Intent
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import pl.edu.uj.ii.skwarczek.betterchatfast.R
+import java.io.IOException
+import java.util.*
+import kotlin.coroutines.CoroutineContext
+
 
 class Onboarding3Fragment : Fragment() {
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+    lateinit var imagePreview: ImageView
+    lateinit var btn_choose_image: Button
+    lateinit var btn_upload_image: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_onboarding_3, container, false)
-
+        initView(view)
+        btn_choose_image.setOnClickListener { launchGallery() }
+        btn_upload_image.setOnClickListener { uploadImage() }
         return view
+    }
+    private fun initView(view: View) {
+        auth = FirebaseAuth.getInstance()
+
+        btn_choose_image = view.findViewById(R.id.btn_choose_image)
+        btn_upload_image = view.findViewById(R.id.btn_upload_image)
+        imagePreview = view.findViewById(R.id.image_preview)
+
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if(data == null || data.data == null){
+                return
+            }
+
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, filePath)
+                imagePreview.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun launchGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+    private fun uploadImage(){
+        if(filePath != null){
+            val ref = storageReference?.child("myImages/" + auth.currentUser!!.uid)
+            val uploadTask = ref?.putFile(filePath!!)
+
+        }else{
+            Toast.makeText(activity, "Please Upload an Image", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/onboarding/Onboarding4Fragment.kt
+++ App/App/src/main/java/pl/edu/uj/ii/skwarczek/betterchatfast/onboarding/Onboarding4Fragment.kt
@@ -1,7 +1,9 @@
 package pl.edu.uj.ii.skwarczek.betterchatfast.onboarding
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,13 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import org.json.JSONObject
 import pl.edu.uj.ii.skwarczek.betterchatfast.R
 import pl.edu.uj.ii.skwarczek.betterchatfast.main.MainActivity
@@ -17,15 +26,29 @@ import pl.edu.uj.ii.skwarczek.betterchatfast.signin.AuthenticateViewModel
 import pl.edu.uj.ii.skwarczek.betterchatfast.util.RequestHandler
 import pl.edu.uj.ii.skwarczek.betterchatfast.util.SENDBIRD_APP_ID
 import pl.edu.uj.ii.skwarczek.betterchatfast.util.FirestoreHelper
+import kotlin.coroutines.CoroutineContext
 
-class Onboarding4Fragment : Fragment() {
+class Onboarding4Fragment : Fragment(), CoroutineScope {
 
     private lateinit var nicknameField: EditText
     private lateinit var firstNameField: EditText
     private lateinit var lastNameField: EditText
+    private lateinit var iconUrlField: EditText
+    private var storageReference: StorageReference? = null
+
     private lateinit var finishOnboardingButton: Button
     private lateinit var auth: FirebaseAuth
     private val viewModel: AuthenticateViewModel = AuthenticateViewModel()
+
+    private var job: Job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +63,7 @@ class Onboarding4Fragment : Fragment() {
             val nickname = nicknameField.text?.trim().toString()
             val firstName = firstNameField.text?.trim().toString()
             val lastName = lastNameField.text?.trim().toString()
+            val iconUrl = iconUrlField.text?.trim().toString()
 
             if (nickname.isNotEmpty() && firstName.isNotEmpty() && lastName.isNotEmpty()) {
                 FirestoreHelper.updateCurrentUserNickname(nickname)
@@ -47,17 +71,27 @@ class Onboarding4Fragment : Fragment() {
                 FirestoreHelper.updateCurrentUserLastName(lastName)
                 FirestoreHelper.updateCurrentUserIsAfterOnboarding(true)
 
+
+                val ref = storageReference?.child("myImages/" + auth.currentUser!!.uid)
                 val mail = auth.currentUser?.email
                 //http post user
-                val url = "https://api-$SENDBIRD_APP_ID.sendbird.com/v3/users"
-                val postJSONObject = JSONObject(
-                    """{"user_id":"$mail",
-                                                "nickname":"$nickname",
-                                                "profile_url":"https://sendbird.com/main/img/profiles/profile_05_512px.png"}"""
-                )
+                var iconPath: Uri? =null
+                launch(Dispatchers.Main) {
+                    iconPath = ref?.downloadUrl?.await()
+                    Log.d("XD", iconPath.toString())
+
+
+                    val url = "https://api-$SENDBIRD_APP_ID.sendbird.com/v3/users"
+                    val postJSONObject = JSONObject(
+                        """{"user_id":"$mail",
+                         "nickname":"$nickname",
+                 "profile_url":"$iconPath"}"""
+                    )
+
                 Thread(kotlinx.coroutines.Runnable {
                     RequestHandler.requestPOST(url, postJSONObject)
                 }).start()
+                }
 
                 val intent = Intent(context, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -77,6 +111,9 @@ class Onboarding4Fragment : Fragment() {
         nicknameField = view.findViewById(R.id.onboarding_4_nickname_edit_text)
         firstNameField = view.findViewById(R.id.onboarding_4_first_name_edit_text)
         lastNameField = view.findViewById(R.id.onboarding_4_last_name_edit_text)
+        iconUrlField = view.findViewById(R.id.onboarding_4_icon_edit_text)
+        storageReference = FirebaseStorage.getInstance().reference
+
     }
 
 }
